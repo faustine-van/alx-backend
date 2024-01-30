59581b53f751850c4e8c47ceaf17e2219846ae9c
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-30 22:22:16 +0200

    fifo basic directionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,7 +16,7 @@ class BasicCache(BaseCaching):
         """ Add an item in the cache
         """
         if key is None or item is None:
-            pass
+            return
         self.cache_data[key] = item
 
     def get(self, key):
