0b72bb127340775c3120c0b5ac8e02e877e9b8fa
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-30 23:32:15 +0200

    mru caching algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -17,17 +17,23 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         """add an item in cache"""
         if key is None or item is None:
-            return
+            return  # Do nothing if key or item is None
         if key in self.cache_data:
             self.cache_data.pop(key)
+        # check if cache is Full
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key, _ = self.cache_data.popitem(last=True)
-            print(f'DISCARD: {last_key}')
+            # Assign the new item to the dictionary
+            discarded_key, _ = self.cache_data.popitem()
+            print(f'DISCARD: {discarded_key}')
+        # Assign the new item to the dictionary
         self.cache_data[key] = item
+        # Move the key to the end to mark it as the most recently used
+        self.cache_data.move_to_end(key)
 
     def get(self, key):
         """ get an item  by key
         """
         if key is None or key not in self.cache_data:
             return None
+        self.cache_data.move_to_end(key)
         return self.cache_data[key]
