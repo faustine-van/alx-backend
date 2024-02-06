5ebbafb7d0de3db03f501d4274f803e07a8ba823
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-07 01:55:51 +0200

    updated

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,7 +4,7 @@
    supported languages
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -32,9 +32,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """set / route"""
-    title = gettext('home_title')
-    say_hello = gettext('home_header')
-    return render_template('0-index.html', title=title, say_hello=say_hello)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
