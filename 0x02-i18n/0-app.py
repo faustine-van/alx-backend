8090e5bdec6b53abebfec0830faee3406a64991a
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-07 01:34:02 +0200

    updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,10 +9,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """set / route"""
-    title = 'Welcome to Holberton'
-    say_hello = 'Hello world'
-    return render_template('0-index.html', title=title, say_hello=say_hello)
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
