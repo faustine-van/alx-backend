a27af09165e9ad2428fbb2443432a4fdcf30b163
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-07 01:40:18 +0200

    updated

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,17 +6,17 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
 
-class Config():
+class Config:
     """intialize configuration
     """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
-babel.init_app(app, default_locale='en', default_timezone='UTC')
 
 
 @app.route('/', strict_slashes=False)
