310bd9b0c84886e1ba764ed6db994b00e6a1f23b
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-07 02:06:50 +0200

    updated

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -34,7 +34,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """set / route"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
